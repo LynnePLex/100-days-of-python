ba462c854fbd028d7b64263b07e272bcc29846d0
LynnePlex <38146775+LynnePLex@users.noreply.github.com>
2021-10-18 14:22:34 -0500

    added day one & code project

---
+++ day_one_Band-name-Gen.py
@@ -0,0 +1,9 @@
+#Intro
+print("Welcome to a band name genarator!
+#Ask the user what city they grew up in.
+city = input("What City you grew up in?")
+#This next question ask the user of their pet name.
+pet = input("What's your pet name?")
+#lastly we combined the city and pet name for the output
+print("Your band name would be: " + city + " " + pet)
+#The output would be Chicago Wynn
